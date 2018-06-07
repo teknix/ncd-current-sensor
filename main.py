99e84b79fd935d8bfc99518392ef572fb12560ab
TeKniX <teknix@code-crew.net>
2018-06-07 10:42:04 -0700

    comments and print out response in decimal

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import socket
 import re
 
-
+# Set NCD Current Monitor IP address 
 TCP_IP = '192.168.1.102'
+# Set Listening Port on Remote Current Monitor
 TCP_PORT = 2101
+# Set Buffer Size
 BUFFER_SIZE = 1024
+
+# Set Command to be sent to Current Monitor
+# In this case query all 6 channels | https://ncd.io/communicating-to-current-monitoring-controllers/
 MESSAGE = 'aa0ebc320a54926a010106000004551374'.decode('hex')
 #MESSAGE = 'AA03FE7C0128'.decode('hex')
-#print MESSAGE
+
+#Create Socket
 s = socket.socket()
 s.connect((TCP_IP, TCP_PORT))
+# Send Message to Current Monitor
 s.send(MESSAGE)
+# Collect response data
 data = s.recv(BUFFER_SIZE)
 s.close()
+
+# Convert response to hex
 data = data.encode('hex')
+#Split into Byte Pairs
 pairs = re.findall('..?', data)
 
 print "received data:", pairs
-print pairs[0]
+
+#
+for k,v in enumerate(pairs):
+    print 'key: ' + k + ' val: ' + int(v, 16)
