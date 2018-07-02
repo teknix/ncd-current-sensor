8a2ebf47e7989d42cb9558bb759f67a08e6ec19c
TeKniX <teknix@code-crew.net>
2018-07-02 10:45:53 -0700

    Decimal to 4

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from socket import error as socket_error
 
 # Set Decimal Precision
 from decimal import *
-getcontext().prec = 5
+getcontext().prec = 4
 
 # Set NCD Current Monitor IP address
 TCP_IP = '192.168.1.102'
