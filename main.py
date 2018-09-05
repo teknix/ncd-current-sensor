1e5846ac020a9ca8484a8c9b608fb094a359d766
teknix <teknix@code-crew.net>
2018-09-05 13:37:43 -0700

    pulled back ifnot running

--- main.py
+++ main.py
@@ -160,15 +160,15 @@ def start_server_monitor():
             channelData = calcAmps(data)
             channelData['time'] = str(datetime.datetime.now())
             ampData = json.dumps(channelData)
-            if channelData['classifier'] > 1 and channelData['classifier'] < 50:
-	            # publish current data
-	            write_mqtt(mqtt_topic, ampData)
+            #if channelData['classifier'] > 1 and channelData['classifier'] < 50:
+            # publish current data
+            write_mqtt(mqtt_topic, ampData)
 
-	            #Save data to MONGODB
-	            mongo = MongoClient(MONGO_IP, MONGO_PORT)
-	            mongoDB = mongo['washline']
-	            ampdata  = mongoDB.amps
-	            ampdata_id = ampdata.insert_one(channelData).inserted_id
+            #Save data to MONGODB
+            mongo = MongoClient(MONGO_IP, MONGO_PORT)
+            mongoDB = mongo['washline']
+            ampdata  = mongoDB.amps
+            ampdata_id = ampdata.insert_one(channelData).inserted_id
             
             time.sleep(sleep_time)
 
